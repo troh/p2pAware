38b1c6369d41fa19c38917901594fbdd91547d5c
tom <tohaga@outlook.com>
2013-11-27 01:51:12 +0000

    Added DefaultActionExecutor.

---
+++ p2pAware/src/com/troh/aware/eca/DefaultActionExecutor.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.troh.aware.eca;
+
+/**
+ * @author tom
+ *
+ */
+public class DefaultActionExecutor implements ActionExecutor {
+
+	/* (non-Javadoc)
+	 * @see com.troh.aware.eca.ActionExecutor#execute(java.lang.String)
+	 */
+	@Override
+	public void execute(String action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.troh.aware.eca.ActionExecutor#execute(java.lang.String, java.lang.Object[])
+	 */
+	@Override
+	public void execute(String action, Object[] data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.troh.aware.eca.ActionExecutor#addExecutionObject(java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public void addExecutionObject(String identifier, Object executor) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
